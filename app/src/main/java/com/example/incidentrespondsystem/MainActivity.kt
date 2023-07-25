bd083110ae26c95c6e26926935ed2442a1f5d34b
jkulei <132292848+jkulei@users.noreply.github.com>
2023-07-25 06:00:53 +0300

    dashboard adition

---
+++ app/src/main/java/com/example/incidentrespondsystem/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.incidentrespondsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
